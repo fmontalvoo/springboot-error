efd9101ad71fd1013ccb3c434490307db98c310a
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-08 13:12:24 -0500

    Initial commit
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ src/test/java/com/fmontalvoo/springboot/error/app/SpringbootErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmontalvoo.springboot.error.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
